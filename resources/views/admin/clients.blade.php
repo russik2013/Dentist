617e1b37b4f286c34ccdf29e982f3c158bb35869
User <russik.drug@yandex.ru>
2017-05-10 19:10:03 +0300

    add client list

--- resources/views/admin/clients.blade.php
+++ resources/views/admin/clients.blade.php
@@ -7,5 +7,23 @@
  */
 
 ?>
-all clients
-{{$clients}}
+<link href="{{asset('dantist/table.css')}}" rel="stylesheet">
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="center">
+        <span class="letter">All clients</span>
+<table>
+
+    @foreach($clients as $item)
+
+        <tr>
+            <td>{{$item->name}}</td>
+        </tr>
+
+    @endforeach
+
+</table>
+    </div>
+@endsection
